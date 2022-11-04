98e54785b3110b77728e20bae3cda9ddf5d1f363
Durval Printes <durvalprintes@gmail.com>
2022-11-04 07:48:39 -0300

    Adicionando uma configuracao de seguranca

---
+++ src/main/java/com/ufopa/spring/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.ufopa.spring.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.stream.Stream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  enum ROLES {
+    LEITURA, ESCRITA;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsManager() {
+    var user = User.builder()
+        .username("user")
+        .password(passwordEncoder().encode("minicurso"))
+        .roles(ROLES.LEITURA.name())
+        .build();
+
+    var admin = User.builder()
+        .username("admin")
+        .password(passwordEncoder().encode("springboot"))
+        .roles(Stream.of(ROLES.values()).map(Enum::name).toArray(String[]::new))
+        .build();
+
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeRequests(auth -> {
+          auth.antMatchers("/home/hello").permitAll();
+          auth.antMatchers(HttpMethod.GET).hasRole(ROLES.LEITURA.name());
+          auth.anyRequest().hasRole(ROLES.ESCRITA.name());
+        })
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .httpBasic(withDefaults())
+        .build();
+  }
+
+}
--- src/main/java/com/ufopa/spring/controller/HomeController.java
+++ src/main/java/com/ufopa/spring/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.ufopa.spring.controller;
 
+import java.security.Principal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/home")
 public class HomeController {
 
+  @Value("${home.hello}")
+  private String mensagem;
+
   @GetMapping(value = "/hello")
-  String hello() {
-    return "Hello World!";
+  String hello(Principal principal) {
+    return "Hello, " + (Optional.ofNullable(principal).isPresent() ? principal.getName() : mensagem);
   }
 
 }
